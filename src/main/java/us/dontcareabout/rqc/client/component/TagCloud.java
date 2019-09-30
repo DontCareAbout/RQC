b9b41122f37faee6d4ec5a75788a466a110f61d0
MontyPan <monty.pan@dontcareabout.us>
2019-10-01 00:14:58 +0800

    增加：可切換排序條件（數量大小 or 字母順序）

--- src/main/java/us/dontcareabout/rqc/client/component/TagCloud.java
+++ src/main/java/us/dontcareabout/rqc/client/component/TagCloud.java
@@ -7,6 +7,8 @@ import java.util.HashMap;
 
 import com.google.common.collect.Lists;
 import com.sencha.gxt.chart.client.draw.RGB;
+import com.sencha.gxt.chart.client.draw.sprite.SpriteSelectionEvent;
+import com.sencha.gxt.chart.client.draw.sprite.SpriteSelectionEvent.SpriteSelectionHandler;
 
 import us.dontcareabout.gxt.client.draw.LayerContainer;
 import us.dontcareabout.gxt.client.draw.component.TextButton;
@@ -14,22 +16,33 @@ import us.dontcareabout.rqc.client.data.Quote;
 import us.dontcareabout.rqc.client.gf.VerticalLayoutLayer;
 
 public class TagCloud extends LayerContainer {
+	private static final String BY_ALPHABETICAL = "Ａ↓";
+	private static final String BY_AMOUNTL = "９↓";
 	private final int baseHeight = 30;
-	private final Comparator<String> comparator = new Comparator<String>() {
-		@Override
-		public int compare(String o1, String o2) {
-			//數字越大在越前面
-			int amountResult = tagMap.get(o2).compareTo(tagMap.get(o1));
-			if (amountResult != 0) { return amountResult; }
-			return o1.compareTo(o2);
-		}
-	};
+	private final SortComparator comparator = new SortComparator();
 
 	private HashMap<String, Integer> tagMap = new HashMap<>();
+
+	/**
+	 * true 表示字母順序優先，false 表示數量優先
+	 */
+	private boolean type;
+	private int min;
+	private int diff;
+
 	private VerticalLayoutLayer vll = new VerticalLayoutLayer();
+	private SortButton sortBtn = new SortButton();
+
+	public TagCloud() {
+		addLayer(sortBtn);
+
+		vll.setMargin(1);
+		vll.setGap(1);
+		vll.setLY(baseHeight + 10);
+		addLayer(vll);
+	}
 
 	public void refresh(ArrayList<Quote> data) {
-		clear();
 		tagMap.clear();
 
 		for (Quote q : data) {
@@ -46,24 +59,45 @@ public class TagCloud extends LayerContainer {
 			}
 		}
 
+		boolean typeTmp = type;	//為了以後重複 load 資料作準備
+		type = false;
+		ArrayList<String> tagList = Lists.newArrayList(tagMap.keySet());
+		Collections.sort(tagList, comparator);
+		min = tagMap.get(tagList.get(tagList.size() - 1));
+		diff = tagMap.get(tagList.get(0)) - min;
+		type = typeTmp;
+
+		rebuildUI();
+	}
+
+	private void rebuildUI() {
 		ArrayList<String> tagList = Lists.newArrayList(tagMap.keySet());
 		Collections.sort(tagList, comparator);
 
-		vll = new VerticalLayoutLayer();
-		vll.setMargin(1);
-		vll.setGap(1);
-
-		int min = tagMap.get(tagList.get(tagList.size() - 1));
-		int diff = tagMap.get(tagList.get(0)) - min;
+		vll.clear();
 
 		for (String tag : tagList) {
 			vll.addChild(new TagButton(tag), ((1.0 * tagMap.get(tag) - min) / diff + 1) * baseHeight);
 		}
 
-		addLayer(vll);
+		vll.redeploy();
 		vll.resize(this.getOffsetWidth(), 1);	//高度不重要 XD
 
-		setPixelSize(this.getOffsetWidth(), (int)vll.getTotalSize());
+		setPixelSize(this.getOffsetWidth(), (int)vll.getTotalSize() + baseHeight + 10);
+	}
+
+	private void changeType() {
+		type = !type;
+		sortBtn.refresh();
+		rebuildUI();
+	}
+
+	@Override
+	protected void onResize(int width, int height) {
+		sortBtn.setLY(5);
+		sortBtn.setLX(10);
+		sortBtn.resize(width - 20, baseHeight);
+		super.onResize(width, height);
 	}
 
 	private class TagButton extends TextButton {
@@ -71,6 +105,39 @@ public class TagCloud extends LayerContainer {
 			super(text);
 			setBgColor(RGB.LIGHTGRAY);
 			setBgRadius(5);
+		}
+	}
+
+	private class SortButton extends TextButton {
+		SortButton() {
+			setBgColor(RGB.YELLOW);
+			setTextColor(RGB.BLUE);
+			refresh();
+			addSpriteSelectionHandler(new SpriteSelectionHandler() {
+				@Override
+				public void onSpriteSelect(SpriteSelectionEvent event) {
+					changeType();
+				}
+			});
+		}
+
+		void refresh() {
+			setText(type ? BY_AMOUNTL : BY_ALPHABETICAL);
+		}
+	}
+
+	private class SortComparator implements Comparator<String> {
+		@Override
+		public int compare(String o1, String o2) {
+			//數字越大在越前面
+			int amountResult = tagMap.get(o2).compareTo(tagMap.get(o1));
+			int stringResult = o1.compareTo(o2);
+
+			if (type) {
+				return stringResult != 0 ? stringResult : amountResult;
+			} else {
+				return amountResult != 0 ? amountResult : stringResult;
+			}
 		}
 	}
 }
