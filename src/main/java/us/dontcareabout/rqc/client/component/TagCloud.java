8a7ede993d5e82c6efcecf2aaba956b2c9160907
MontyPan <monty.pan@dontcareabout.us>
2019-10-02 00:20:17 +0800

    增加：「選取 tag」、「設定多個 tag 組合條件」
    SortButton / ConditionButton 顯示字串邏輯統一改成「當下狀態」

--- src/main/java/us/dontcareabout/rqc/client/component/TagCloud.java
+++ src/main/java/us/dontcareabout/rqc/client/component/TagCloud.java
@@ -13,15 +13,21 @@ import com.sencha.gxt.chart.client.draw.sprite.SpriteSelectionEvent.SpriteSelect
 import us.dontcareabout.gxt.client.draw.LayerContainer;
 import us.dontcareabout.gxt.client.draw.component.TextButton;
 import us.dontcareabout.rqc.client.data.Quote;
+import us.dontcareabout.rqc.client.gf.HorizontalLayoutLayer;
 import us.dontcareabout.rqc.client.gf.VerticalLayoutLayer;
+import us.dontcareabout.rqc.client.ui.TagConditionChangeEvent;
+import us.dontcareabout.rqc.client.ui.UiCenter;
 
 public class TagCloud extends LayerContainer {
 	private static final String BY_ALPHABETICAL = "Ａ↓";
 	private static final String BY_AMOUNTL = "９↓";
+	private static final String BY_AND = "AND";
+	private static final String BY_OR = "O R";	//TODO 等 GF 修正完 TextButton.setText() 的重新置中問題
+
 	private final int baseHeight = 30;
 	private final SortComparator comparator = new SortComparator();
 
-	private HashMap<String, Integer> tagMap = new HashMap<>();
+	private HashMap<String, TagStatus> tagMap = new HashMap<>();
 
 	/**
 	 * true 表示字母順序優先，false 表示數量優先
@@ -31,10 +37,16 @@ public class TagCloud extends LayerContainer {
 	private int diff;
 
 	private VerticalLayoutLayer vll = new VerticalLayoutLayer();
+	private HorizontalLayoutLayer hll = new HorizontalLayoutLayer();
 	private SortButton sortBtn = new SortButton();
+	private ConditionButton conditionBtn = new ConditionButton();
 
 	public TagCloud() {
-		addLayer(sortBtn);
+		hll.addChild(sortBtn, 0.5);;
+		hll.addChild(conditionBtn, 0.5);
+		hll.setMargin(1);
+		hll.setGap(4);
+		addLayer(hll);
 
 		vll.setMargin(1);
 		vll.setGap(1);
@@ -48,14 +60,14 @@ public class TagCloud extends LayerContainer {
 		for (Quote q : data) {
 			for (String tag : q.getTag()) {
 				tag = tag.substring(0, 1).toUpperCase() + tag.substring(1).toLowerCase();
-				Integer amount = tagMap.get(tag);
+				TagStatus status = tagMap.get(tag);
 
-				if (amount == null) {
-					amount = 0;
+				if (status == null) {
+					status = new TagStatus();;
+					tagMap.put(tag, status);
 				}
 
-				amount++;
-				tagMap.put(tag, amount);
+				status.amount++;
 			}
 		}
 
@@ -63,11 +75,17 @@ public class TagCloud extends LayerContainer {
 		type = false;
 		ArrayList<String> tagList = Lists.newArrayList(tagMap.keySet());
 		Collections.sort(tagList, comparator);
-		min = tagMap.get(tagList.get(tagList.size() - 1));
-		diff = tagMap.get(tagList.get(0)) - min;
+		min = tagMap.get(tagList.get(tagList.size() - 1)).amount;
+		diff = tagMap.get(tagList.get(0)).amount - min;
 		type = typeTmp;
 
 		rebuildUI();
+	}
+
+	@Override
+	protected void onResize(int width, int height) {
+		hll.resize(width, baseHeight);
+		super.onResize(width, height);
 	}
 
 	private void rebuildUI() {
@@ -77,7 +95,8 @@ public class TagCloud extends LayerContainer {
 		vll.clear();
 
 		for (String tag : tagList) {
-			vll.addChild(new TagButton(tag), ((1.0 * tagMap.get(tag) - min) / diff + 1) * baseHeight);
+			TagStatus status = tagMap.get(tag);
+			vll.addChild(new TagButton(tag, status.select), ((1.0 * status.amount - min) / diff + 1) * baseHeight);
 		}
 
 		vll.redeploy();
@@ -92,19 +111,28 @@ public class TagCloud extends LayerContainer {
 		rebuildUI();
 	}
 
-	@Override
-	protected void onResize(int width, int height) {
-		sortBtn.setLY(5);
-		sortBtn.setLX(10);
-		sortBtn.resize(width - 20, baseHeight);
-		super.onResize(width, height);
-	}
-
 	private class TagButton extends TextButton {
-		TagButton(String text) {
+		boolean select;
+
+		TagButton(String text, boolean isSelect) {
 			super(text);
-			setBgColor(RGB.LIGHTGRAY);
+			select = isSelect;
 			setBgRadius(5);
+			refresh();
+			addSpriteSelectionHandler(new SpriteSelectionHandler() {
+				@Override
+				public void onSpriteSelect(SpriteSelectionEvent event) {
+					select = !select;
+					refresh();
+					tagMap.get(getText()).select = select;
+					UiCenter.selectTagChange(getText(), select);
+				}
+			});
+		}
+
+		void refresh() {
+			setBgColor(select ? RGB.RED : RGB.LIGHTGRAY);
+			setTextColor(select ? RGB.WHITE : RGB.BLACK);
 		}
 	}
 
@@ -122,7 +150,30 @@ public class TagCloud extends LayerContainer {
 		}
 
 		void refresh() {
-			setText(type ? BY_AMOUNTL : BY_ALPHABETICAL);
+			setText(type ? BY_ALPHABETICAL : BY_AMOUNTL);
+		}
+	}
+
+	private class ConditionButton extends TextButton {
+		/** @see TagConditionChangeEvent#value */
+		boolean condition;
+
+		ConditionButton() {
+			setBgColor(RGB.YELLOW);
+			setTextColor(RGB.BLUE);
+			refresh();
+			addSpriteSelectionHandler(new SpriteSelectionHandler() {
+				@Override
+				public void onSpriteSelect(SpriteSelectionEvent event) {
+					condition = !condition;
+					refresh();
+					UiCenter.tagConditionChange(condition);
+				}
+			});
+		}
+
+		void refresh() {
+			setText(condition ? BY_AND : BY_OR);
 		}
 	}
 
@@ -130,7 +181,7 @@ public class TagCloud extends LayerContainer {
 		@Override
 		public int compare(String o1, String o2) {
 			//數字越大在越前面
-			int amountResult = tagMap.get(o2).compareTo(tagMap.get(o1));
+			int amountResult = tagMap.get(o2).amount.compareTo(tagMap.get(o1).amount);
 			int stringResult = o1.compareTo(o2);
 
 			if (type) {
@@ -139,5 +190,10 @@ public class TagCloud extends LayerContainer {
 				return amountResult != 0 ? amountResult : stringResult;
 			}
 		}
+	}
+
+	private class TagStatus {
+		Integer amount = 0;
+		boolean select;
 	}
 }
