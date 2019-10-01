c6c9f9ac10fa24cd4c021bbda3215061dec57f25
MontyPan <monty.pan@dontcareabout.us>
2019-10-02 00:25:26 +0800

    處理 tag 過濾條件

--- src/main/java/us/dontcareabout/rqc/client/component/QuoteGrid.java
+++ src/main/java/us/dontcareabout/rqc/client/component/QuoteGrid.java
@@ -1,6 +1,7 @@
 package us.dontcareabout.rqc.client.component;
 
 import java.util.ArrayList;
+import java.util.Set;
 
 import com.google.common.base.Strings;
 import com.google.gwt.cell.client.AbstractCell;
@@ -11,6 +12,8 @@ import com.sencha.gxt.core.client.ValueProvider;
 import com.sencha.gxt.data.shared.ListStore;
 import com.sencha.gxt.data.shared.ModelKeyProvider;
 import com.sencha.gxt.data.shared.PropertyAccess;
+import com.sencha.gxt.data.shared.Store;
+import com.sencha.gxt.data.shared.Store.StoreFilter;
 import com.sencha.gxt.widget.core.client.grid.ColumnConfig;
 import com.sencha.gxt.widget.core.client.grid.ColumnModel;
 import com.sencha.gxt.widget.core.client.grid.GridView;
@@ -20,6 +23,11 @@ import com.sencha.gxt.widget.core.client.grid.RowExpander;
 import us.dontcareabout.gxt.client.model.GetValueProvider;
 import us.dontcareabout.rqc.client.data.Quote;
 import us.dontcareabout.rqc.client.gf.Grid2;
+import us.dontcareabout.rqc.client.ui.SelectTagChangeEvent;
+import us.dontcareabout.rqc.client.ui.SelectTagChangeEvent.SelectTagChangeHandler;
+import us.dontcareabout.rqc.client.ui.TagConditionChangeEvent;
+import us.dontcareabout.rqc.client.ui.TagConditionChangeEvent.TagConditionChangeHandler;
+import us.dontcareabout.rqc.client.ui.UiCenter;
 
 public class QuoteGrid extends Grid2<Quote> {
 	private static Properties properties = GWT.create(Properties.class);
@@ -55,9 +63,25 @@ public class QuoteGrid extends Grid2<Quote> {
 		}
 	);
 
+	private Filter filter = new Filter();
+
 	public QuoteGrid() {
 		init();
 		rowExpander.initPlugin(this);
+		UiCenter.addSelectTagChange(new SelectTagChangeHandler() {
+			@Override
+			public void onSelectTag(SelectTagChangeEvent event) {
+				filter.tagSet = event.data;
+				adjustFilter();
+			}
+		});
+		UiCenter.addTagConditionChange(new TagConditionChangeHandler() {
+			@Override
+			public void onConditionChange(TagConditionChangeEvent event) {
+				filter.condition = event.value;
+				adjustFilter();
+			}
+		});
 	}
 
 	public void refresh(ArrayList<Quote> data) {
@@ -90,7 +114,9 @@ public class QuoteGrid extends Grid2<Quote> {
 
 	@Override
 	protected ListStore<Quote> genListStore() {
-		return new ListStore<>(properties.key());
+		ListStore<Quote> result = new ListStore<>(properties.key());
+		result.addFilter(filter);
+		return result;
 	}
 
 	@Override
@@ -102,6 +128,11 @@ public class QuoteGrid extends Grid2<Quote> {
 		return view;
 	}
 
+	private void adjustFilter() {
+		getStore().setEnableFilters(false);
+		getStore().setEnableFilters(true);
+	}
+
 	interface Properties extends PropertyAccess<Quote> {
 		@Path("index") ModelKeyProvider<Quote> key();
 		ValueProvider<Quote, String> type();
@@ -110,5 +141,41 @@ public class QuoteGrid extends Grid2<Quote> {
 		ValueProvider<Quote, String> text();
 		ValueProvider<Quote, String> note();
 		ValueProvider<Quote, Double> score();
+	}
+
+	private class Filter implements StoreFilter<Quote> {
+		/** @see SelectTagChangeEvent#data */
+		Set<String> tagSet;
+
+		/** @see TagConditionChangeEvent#value */
+		boolean condition;
+
+		@Override
+		public boolean select(Store<Quote> store, Quote parent, Quote item) {
+			if (tagSet.isEmpty()) { return true; }
+
+			if (condition) {
+				//直接用空間換取程式行數 XD
+				ArrayList<String> upperCaseTag = new ArrayList<>();
+
+				for (String tag : item.getTag()) {
+					upperCaseTag.add(tag.toUpperCase());
+				}
+
+				for (String conditionTag : tagSet) {
+					if (!upperCaseTag.contains(conditionTag)) {
+						return false;
+					}
+				}
+
+				return true;
+			} else {
+				for (String tag : item.getTag()) {
+					if (tagSet.contains(tag.toUpperCase())) { return true; }
+				}
+
+				return false;
+			}
+		}
 	}
 }
