6145161d077ea6b711ce0c4b2bfb62b50c202084
MontyPan <monty.pan@dontcareabout.us>
2019-10-03 00:39:37 +0800

    增加：控制 ui 的基礎 util

--- src/main/java/us/dontcareabout/rqc/client/ui/UiCenter.java
+++ src/main/java/us/dontcareabout/rqc/client/ui/UiCenter.java
@@ -4,6 +4,10 @@ import java.util.HashSet;
 
 import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.event.shared.SimpleEventBus;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.widget.core.client.Window;
+import com.sencha.gxt.widget.core.client.container.Viewport;
 
 import us.dontcareabout.rqc.client.ui.SelectTagChangeEvent.SelectTagChangeHandler;
 import us.dontcareabout.rqc.client.ui.TagConditionChangeEvent.TagConditionChangeHandler;
@@ -39,5 +43,42 @@ public class UiCenter {
 
 	public static HandlerRegistration addTagConditionChange(TagConditionChangeHandler handler) {
 		return eventBus.addHandler(TagConditionChangeEvent.TYPE, handler);
+	}
+
+	////////////////////////////////
+
+	public static void start() {
+		viewport.add(new QuoteView());
+	}
+
+	private final static Viewport viewport = new Viewport();
+	static {
+		RootPanel.get().add(viewport);
+	}
+
+	public static void mask(String message) {
+		viewport.mask(message);
+	}
+
+	public static void unmask() {
+		viewport.unmask();
+	}
+
+	private final static Window dialog = new Window();
+	static {
+		dialog.setModal(true);
+		dialog.setResizable(false);
+	}
+
+	public static void closeDialog() {
+		dialog.hide();
+	}
+
+	private static void dialog(Widget widget, int width, int height) {
+		dialog.clear();
+		dialog.add(widget);
+		dialog.show();
+		dialog.setPixelSize(width, height);
+		dialog.center();
 	}
 }
