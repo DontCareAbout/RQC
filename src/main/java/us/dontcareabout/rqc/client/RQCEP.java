14e5fb72ebcbd87f914ac928b5268b799be3670b
MontyPan <monty.pan@dontcareabout.us>
2020-03-05 23:20:05 +0800

    entry point 終於有東西可以執行了... [被毆飛]

--- src/main/java/us/dontcareabout/rqc/client/RQCEP.java
+++ src/main/java/us/dontcareabout/rqc/client/RQCEP.java
@@ -3,9 +3,15 @@ package us.dontcareabout.rqc.client;
 import com.google.gwt.user.client.Window;
 
 import us.dontcareabout.gwt.client.GFEP;
+import us.dontcareabout.gwt.client.iCanUse.Feature;
+import us.dontcareabout.rqc.client.data.DataCenter;
+import us.dontcareabout.rqc.client.data.SheetId;
+import us.dontcareabout.rqc.client.ui.UiCenter;
 
 public class RQCEP extends GFEP {
-	public RQCEP() {}
+	public RQCEP() {
+		needFeature(Feature.Storage);
+	}
 
 	@Override
 	protected String version() { return "0.0.1"; }
@@ -20,5 +26,7 @@ public class RQCEP extends GFEP {
 
 	@Override
 	protected void start() {
+		UiCenter.start();
+		DataCenter.wantQuote(SheetId.get());
 	}
 }
